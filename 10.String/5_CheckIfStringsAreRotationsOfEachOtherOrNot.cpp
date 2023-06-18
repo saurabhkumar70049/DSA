039f70db8331429d60b77236148e1ef5a264f5ef
Saurabh Kumar <85560264+saurabhkumar70049@users.noreply.github.com>
2023-06-18 10:50:07 +0530

    "done"

--- 10.String/5_CheckIfStringsAreRotationsOfEachOtherOrNot.cpp
+++ 10.String/5_CheckIfStringsAreRotationsOfEachOtherOrNot.cpp
@@ -39,31 +39,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void swapElement(string &str2, int low, int high) {
-    while(low < high) {
-        int temp = str2[low];
-        str2[low] = str2[high];
-        str2[high] = temp;
-        low++,
-        high--;
-    }
-}
 
-string rotatedString(string &str2, int i) {
-    swapElement(str2, 0, i-1);
-    swapElement(str2, i, str2.length()-1);
-    swapElement(str2, 0, str2.length()-1);
-    cout << str2 <<endl;
-    return str2;
-}
 
-bool checkRotation(string &str1, string &str2) {
-    for(int i = 0; i < str2.length(); i++) {
-        if(str2 == rotatedString(str1, 1)){
-            return true;
-        }
+//In this solution, I right sift the element of second array and then comapare to first array
+// void swapElement(string &str2, int low, int high) { 
+//     while(low < high) {
+//         int temp = str2[low];
+//         str2[low] = str2[high];
+//         str2[high] = temp;
+//         low++,
+//         high--;
+//     }
+// }
+
+// string rotatedString(string &str2, int i) {
+//     swapElement(str2, 0, i-1);
+//     swapElement(str2, i, str2.length()-1);
+//     swapElement(str2, 0, str2.length()-1);
+//     cout << str2 <<endl;
+//     return str2;
+// }
+
+// bool checkRotation(string &str1, string &str2) {
+//     for(int i = 0; i < str2.length(); i++) {
+//         if(str2 == rotatedString(str1, 1)){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+
+//In this method, I compare both the string
+// bool checkRotation(string str1, string str2) {
+//     if(str1.length() != str2.length()) {
+//         return false;
+//     }
+//     int count = 1;
+//     for(int i = 0; i < str1.length(); i++) {
+//         if(str1[i] == str2[0]) {
+//             bool check = true;
+//             for(int j = 1; j < str2.length(); j++) {
+//                 if(str2[j] != str1[(j+i)%str1.length()]){
+//                     check = false;
+//                     break;
+//                 }
+//             }
+//             if(check == true) {
+//                 return true;
+//             }
+//         }
+
+//     }
+//     return false;
+// }
+
+
+
+//In this method, Add first string with itself then find out string second in it
+bool checkRotation(string str1, string str2) {
+    string temp = str1+str1;
+    if(temp.find(str2)!=string::npos) {   //stiring::npos is indicate the absence of string as substring and part of string 
+        cout << temp.find(str2) <<endl;
+        return true;
     }
-    return false;
+    else {
+        return false;
+    }
 }
 
 
